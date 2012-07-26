aa5386a60e24392da01b2ced78f885d8355eb196
hufsa <isak.mortzell@gmail.com>
2012-07-26 02:02:38 -0400

    Core: Define and implement some spell attributes.

--- src/server/game/Spells/Spell.cpp
+++ src/server/game/Spells/Spell.cpp
@@ -335,7 +335,7 @@ Spell::Spell(Unit* Caster, SpellEntry const *info, bool triggered, uint64 origin
 
     m_castPositionX = m_castPositionY = m_castPositionZ = 0;
     m_TriggerSpells.clear();
-    m_IsTriggeredSpell = triggered;
+    m_IsTriggeredSpell = bool(triggered || (info->AttributesEx4 & SPELL_ATTR_EX4_FORCE_TRIGGERED));
     //m_AreaAura = false;
     m_CastItem = NULL;
 
@@ -360,9 +360,9 @@ Spell::Spell(Unit* Caster, SpellEntry const *info, bool triggered, uint64 origin
 
     if (m_spellInfo->DmgClass == SPELL_DAMAGE_CLASS_MAGIC
         && !IsAreaOfEffectSpell(m_spellInfo)
-        && (m_spellInfo->AttributesEx2 & SPELL_ATTR_EX2_CANT_REFLECTED) == 0)
+        && (m_spellInfo->AttributesEx2 & SPELL_ATTR_EX_CANT_REFLECTED) == 0)
     {
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j < MAX_SPELL_EFFECTS; j++)
         {
             if (m_spellInfo->Effect[j] == 0)
                 continue;
@@ -1361,8 +1361,7 @@ void Spell::SearchChainTarget(std::list<Unit*> &TagUnitMap, float max_range, uin
         if (TargetType == SPELL_TARGETS_CHAINHEAL)
         {
             next = tempUnitMap.begin();
-            while (cur->GetDistance(*next) > CHAIN_SPELL_JUMP_RADIUS
-                || !cur->IsWithinLOSInMap(*next))
+            while (cur->GetDistance(*next) > CHAIN_SPELL_JUMP_RADIUS || (!(m_spellInfo->AttributesEx2 & SPELL_ATTR_EX2_IGNORE_LOS) && !cur->IsWithinLOSInMap(*next)))
             {
                 ++next;
                 if (next == tempUnitMap.end())
@@ -1379,7 +1378,7 @@ void Spell::SearchChainTarget(std::list<Unit*> &TagUnitMap, float max_range, uin
             while (m_spellInfo->DmgClass == SPELL_DAMAGE_CLASS_MELEE
                 && !m_caster->isInFrontInMap(*next, max_range)
                 || !m_caster->canSeeOrDetect(*next, false)
-                || !cur->IsWithinLOSInMap(*next))
+                || (!(m_spellInfo->AttributesEx2 & SPELL_ATTR_EX2_IGNORE_LOS) && !cur->IsWithinLOSInMap(*next)))
             {
                 ++next;
                 if (next == tempUnitMap.end() || cur->GetDistance(*next) > CHAIN_SPELL_JUMP_RADIUS)
@@ -3564,7 +3563,7 @@ uint8 Spell::CanCast(bool strict)
             if (target->isInFlight())
                 return SPELL_FAILED_BAD_TARGETS;
 
-            if (!m_IsTriggeredSpell && VMAP::VMapFactory::checkSpellForLoS(m_spellInfo->Id) && !m_caster->IsWithinLOSInMap(target))
+            if (!m_IsTriggeredSpell && !(m_spellInfo->AttributesEx2 & SPELL_ATTR_EX2_IGNORE_LOS) && !m_caster->IsWithinLOSInMap(target))
                 return SPELL_FAILED_LINE_OF_SIGHT;
 
             // auto selection spell rank implemented in WorldSession::HandleCastSpellOpcode
@@ -5255,7 +5254,7 @@ bool Spell::CheckTarget(Unit* target, uint32 eff)
     }
 
     //Do not check LOS for triggered spells
-    if (m_IsTriggeredSpell || m_spellInfo->Id == 33395)
+    if (m_spellInfo->AttributesEx2 & SPELL_ATTR_EX2_IGNORE_LOS || m_spellInfo->Id == 33395)
         return true;
 
     //Check targets for LOS visibility (except spells without range limitations)

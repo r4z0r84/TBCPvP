6def0b2638c64dd8250ac7a48a089c342e76c002
Henry <henry925@gmail.com>
2012-07-26 02:36:16 -0400

    Core/Models: Fix incorrect model ID for travel form. Old bug.

--- src/server/game/Spells/Auras/SpellAuras.cpp
+++ src/server/game/Spells/Auras/SpellAuras.cpp
@@ -2550,7 +2550,7 @@ void Aura::HandleAuraModShapeshift(bool apply, bool Real)
             PowerType = POWER_ENERGY;
             break;
         case FORM_TRAVEL:
-            modelid = 632;
+            modelid = 918;
             break;
         case FORM_AQUA:
             if (Player::TeamForRace(m_target->getRace()) == ALLIANCE)

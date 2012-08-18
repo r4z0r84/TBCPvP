ccfd22bb677e136e498a144a113d5a96b544225c
hufsa <isak.mortzell@gmail.com>
2012-08-18 05:19:01 +0200

    Core/Spells: Fixed Librams, requested on forums.

---
+++ sql/updates/world/2012_08_18_world_spell_affect.sql
@@ -0,0 +1,10 @@
+DELETE FROM `spell_affect` WHERE `entry` IN (32403, 28851, 28853);
+
+-- Blessed Book of Nagrand
+INSERT INTO `spell_affect` VALUES (32403, 0, 0x0000000040000000);
+
+-- Libram of Light
+INSERT INTO `spell_affect` VALUES (28851, 0, 0x0000000040000000);
+
+-- Libram of Divinity
+INSERT INTO `spell_affect` VALUES (28853, 0, 0x0000000040000000);

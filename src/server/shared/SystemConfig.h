948b4c48aeab68917bd4692bb6b0be253afc5cd8
Henry <henry925@gmail.com>
2013-01-04 15:00:11 -0500

    Core/SystemConfig: Remove Skyfire from server information just for privacy reasons.

--- src/server/shared/SystemConfig.h
+++ src/server/shared/SystemConfig.h
@@ -23,7 +23,7 @@
 #include "Define.h"
 #include "revision.h"
 
-#define _PACKAGENAME "SkyFireONE"
+#define _PACKAGENAME "Smolderforge"
 
 #if TRINITY_ENDIAN == TRINITY_BIGENDIAN
 # define _ENDIAN_STRING "big-endian"

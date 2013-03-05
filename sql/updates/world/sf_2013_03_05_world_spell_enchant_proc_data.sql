44ef99060b2aef0fb2df8ca02043c937de2d1688
Henry <henry925@gmail.com>
2013-03-05 16:26:13 -0500

    Database/SpellEnchantProc: Add a 1 minute internal cooldown to Mongoose and Executioner based on white attack damage.

---
+++ sql/updates/world/sf_2013_03_05_world_spell_enchant_proc_data.sql
@@ -0,0 +1,3 @@
+REPLACE INTO spell_enchant_proc_data (`entry`, `customChance`, `PPMChance`,`procEx`) VALUES (3225, 0, 1.0, 0);
+
+REPLACE INTO spell_enchant_proc_data (`entry`, `customChance`, `PPMChance`,`procEx`) VALUES (2673, 0, 1.0, 0);

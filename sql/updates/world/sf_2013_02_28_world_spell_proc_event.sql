8b61f4346e31e7e51a7f2d3b4beb820020b5b6f0
Henry <henry925@gmail.com>
2013-03-06 18:16:10 -0500

    Revert: Database/SpellProc: Troll racial Shadowguard can now proc Blackout.

--- sql/updates/world/sf_2013_02_28_world_spell_proc_event.sql
+++
@@ -1,5 +0,0 @@
-UPDATE `spell_proc_event` SET `procFlags`=84376 WHERE  `entry`=15268 LIMIT 1;
-UPDATE `spell_proc_event` SET `procFlags`=84376 WHERE  `entry`=15323 LIMIT 1;
-UPDATE `spell_proc_event` SET `procFlags`=84376 WHERE  `entry`=15324 LIMIT 1;
-UPDATE `spell_proc_event` SET `procFlags`=84376 WHERE  `entry`=15325 LIMIT 1;
-UPDATE `spell_proc_event` SET `procFlags`=84376 WHERE  `entry`=15326 LIMIT 1;

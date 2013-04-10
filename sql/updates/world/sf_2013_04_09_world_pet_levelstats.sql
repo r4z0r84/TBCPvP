f62a9cabb63f2b3aa08a3887e58abf7c69437b55
Henry <henry925@gmail.com>
2013-04-09 20:09:27 -0400

    Database/Pet: Increase Warlock Felguard's stamina and intellect to more correct values.

---
+++ sql/updates/world/sf_2013_04_09_world_pet_levelstats.sql
@@ -0,0 +1,2 @@
+UPDATE `pet_levelstats` SET `sta`=351 WHERE  `creature_entry`=17252 AND `level`=70 LIMIT 1;
+UPDATE `pet_levelstats` SET `inte`=196 WHERE  `creature_entry`=17252 AND `level`=70 LIMIT 1;

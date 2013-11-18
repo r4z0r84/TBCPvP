c540e613f06d48f675d9c928c2b7f76eda7a12ec
Henry Henderson <henry925@gmail.com>
2013-11-18 10:27:16 -0500

    Database/SpellProcEvent: Blackout can now proc from mind flay and Shadow Word: Death.

---
+++ sql/updates/world/sf_2013_11_18_world_spell_proc_event.sql
@@ -0,0 +1,6 @@
+--Blackout can now proc from mind flay and shadow word: Death
+UPDATE `spell_proc_event` SET `SpellFamilyMask`=8699551744 WHERE  `entry`=15326 LIMIT 1;
+UPDATE `spell_proc_event` SET `SpellFamilyMask`=8699551744 WHERE  `entry`=15325 LIMIT 1;
+UPDATE `spell_proc_event` SET `SpellFamilyMask`=8699551744 WHERE  `entry`=15324 LIMIT 1;
+UPDATE `spell_proc_event` SET `SpellFamilyMask`=8699551744 WHERE  `entry`=15323 LIMIT 1;
+UPDATE `spell_proc_event` SET `SpellFamilyMask`=8699551744 WHERE  `entry`=15268 LIMIT 1;

7be980fad4587b09318016ae3712f144c10471a7
Henry <henry925@gmail.com>
2014-12-09 06:36:04 -0500

    Core/Player: Excuse GMs from previous commit (f61e8a1) for testing purposes.

--- src/server/game/Handlers/BattleGroundHandler.cpp
+++ src/server/game/Handlers/BattleGroundHandler.cpp
@@ -95,10 +95,10 @@ void WorldSession::HandleBattleGroundJoinOpcode(WorldPacket & recv_data)
     // can do this, since it's battleground, not arena
     uint32 bgQueueTypeId = sBattleGroundMgr->BGQueueTypeId(bgTypeId, 0);
 
-    if (_player->m_Played_time[PLAYED_TIME_TOTAL] < 900)
+    if (_player->m_Played_time[PLAYED_TIME_TOTAL] < 900 || !player->isGameMaster())
     {
         _player->GetSession()->SendAreaTriggerMessage("You must have at least 15 minutes of played time before you may queue. Please spend a little more time setting up your character, and try again soon. You may check how much time is remaining by typing: /played");
-       ChatHandler(_player).PSendSysMessage("You must have at least 15 minutes of played time before you may queue. Please spend a little more time setting up your character, and try again soon. You may check how much time is remaining by typing: /played");
+        ChatHandler(_player).PSendSysMessage("You must have at least 15 minutes of played time before you may queue. Please spend a little more time setting up your character, and try again soon. You may check how much time is remaining by typing: /played");
         return;
     }
 
@@ -716,10 +716,10 @@ void WorldSession::HandleBattleGroundArenaJoin(WorldPacket & recv_data)
         }
     }
 
-    if (_player->m_Played_time[PLAYED_TIME_TOTAL] < 900)
+    if (_player->m_Played_time[PLAYED_TIME_TOTAL] < 900 || !player->isGameMaster())
     {
         _player->GetSession()->SendAreaTriggerMessage("You must have at least 15 minutes of played time before you may queue. Please spend a little more time setting up your character, and try again soon. You may check how much time is remaining by typing: /played");
-       ChatHandler(_player).PSendSysMessage("You must have at least 15 minutes of played time before you may queue. Please spend a little more time setting up your character, and try again soon. You may check how much time is remaining by typing: /played");
+        ChatHandler(_player).PSendSysMessage("You must have at least 15 minutes of played time before you may queue. Please spend a little more time setting up your character, and try again soon. You may check how much time is remaining by typing: /played");
         return;
     }
 

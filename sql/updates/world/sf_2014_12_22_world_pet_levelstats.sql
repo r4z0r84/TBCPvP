1ccd08a15c3c5525712919e46c2cfa992fcd44be
Henry <henry925@gmail.com>
2014-12-22 23:10:32 -0500

    Database/PetLevelstats: Fix Shadowfiend's health at levels.

---
+++ sql/updates/world/sf_2014_12_22_world_pet_levelstats.sql
@@ -0,0 +1,5 @@
+UPDATE `pet_levelstats` SET `hp`='1796' WHERE (`creature_entry`='19668') AND (`level`='66') ;
+UPDATE `pet_levelstats` SET `hp`='2014' WHERE (`creature_entry`='19668') AND (`level`='67');
+UPDATE `pet_levelstats` SET `hp`='2221' WHERE (`creature_entry`='19668') AND (`level`='68');
+UPDATE `pet_levelstats` SET `hp`='2420' WHERE (`creature_entry`='19668') AND (`level`='69');
+UPDATE `pet_levelstats` SET `hp`='2638' WHERE (`creature_entry`='19668') AND (`level`='70');

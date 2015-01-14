3d79b842f67573efd2394b5b5a48a7bd5a620109
Itslovelol <info@lovelol.de>
2015-01-14 16:26:10 +0100

    Core/TempEventMgr: Implement functions to add teleport location to events.

--- src/server/game/Events/TempEventMgr.cpp
+++ src/server/game/Events/TempEventMgr.cpp
@@ -1,9 +1,5 @@
 #include "TempEventMgr.h"
-
-TempEventMgr::TempEventMgr()
-{
-    m_EventStatus = EVENT_STATUS_INACTIVE;
-}
+#include "MapManager.h"
 
 void TempEventMgr::AddEvent(uint32 eventEntry)
 {
@@ -15,9 +11,30 @@ void TempEventMgr::DeleteEvent(uint32 eventEntry)
     // Delete Event from database
 }
 
-void TempEventMgr::AddEventLocation(uint32 eventEntry, uint32 mapid, float x, float y, float z, float orientation)
+bool TempEventMgr::AddEventLocation(uint32 eventEntry, uint32 mapId, float x, float y, float z, float orientation)
 {
-    // Add teleport location to the event
+    for (TempEventLocation::const_iterator itr = GetTempEventLocationMap().begin(); itr != GetTempEventLocationMap().end(); ++itr)
+    {
+        if (itr->first == eventEntry)
+            return false;
+    }
+
+    EventLocation eventLoc;
+    eventLoc.eventEntry     = eventEntry;
+    eventLoc.mapId          = mapId;
+    eventLoc.x              = x;
+    eventLoc.y              = y;
+    eventLoc.z              = z;
+    eventLoc.orientation    = orientation;
+
+    if (!sMapMgr->IsValidMapCoord(eventLoc.mapId, eventLoc.x, eventLoc.y, eventLoc.z))
+    {
+        // Not valid map coords
+        return false;
+    }
+
+    m_TempEventLocation[eventEntry] = eventLoc;
+    return true;
 }
 
 void TempEventMgr::DeleteEventLocation(uint32 eventEntry)
--- src/server/game/Events/TempEventMgr.h
+++ src/server/game/Events/TempEventMgr.h
@@ -3,6 +3,16 @@
 
 #include "Player.h"
 
+struct EventLocation
+{
+    uint32 eventEntry;
+    uint32 mapId;
+    float x;
+    float y;
+    float z;
+    float orientation;
+};
+
 enum EventStatus
 {
     EVENT_STATUS_INACTIVE         = 0,
@@ -13,13 +23,19 @@ class TempEventMgr
 {
 public:
 
-    TempEventMgr();
+    TempEventMgr() {};
     ~TempEventMgr() {};
 
     void AddEvent(uint32 eventEntry);
     void DeleteEvent(uint32 eventEntry);
 
-    void AddEventLocation(uint32 eventEntry, uint32 mapid, float x, float y, float z, float orientation);
+    typedef std::set<uint16> ActiveTempEvents;
+    ActiveTempEvents const& GetActiveTempEventList() const { return m_ActiveTempEvents; }
+
+    typedef std::map<uint32, EventLocation> TempEventLocation;
+    TempEventLocation const& GetTempEventLocationMap() const { return m_TempEventLocation; }
+
+    bool AddEventLocation(uint32 eventEntry, uint32 mapId, float x, float y, float z, float orientation);
     void DeleteEventLocation(uint32 eventEntry);
 
     void AddPlayerToEvent(uint32 eventEntry, Player* player);
@@ -29,6 +45,9 @@ public:
     void SetEventStatus(uint32 eventStatus) { m_EventStatus = eventStatus; }
 
 private:
+
+    ActiveTempEvents m_ActiveTempEvents;
+    TempEventLocation m_TempEventLocation;
 
     uint32 m_EventStatus;
 

363fb6aba084e4445bd3f732a935c7dd63de07ce
Henry Henderson <henry925@gmail.com>
2015-01-19 15:40:12 -0500

    SQL/TempEvent: Update temporary event SQL with newer commands, levels and help syntax. Rerun on world database!

--- sql/updates/dev_sql/TEMPEVENT/tempevent_commands.sql
+++ sql/updates/dev_sql/TEMPEVENT/tempevent_commands.sql
@@ -1,9 +1,12 @@
-INSERT INTO `command` VALUES ('tempevent leave', 0, NULL);
-INSERT INTO `command` VALUES ('tempevent info', 0, NULL);
-INSERT INTO `command` VALUES ('tempevent gminfo', 2, NULL);
-INSERT INTO `command` VALUES ('tempevent create', 2, NULL);
-INSERT INTO `command` VALUES ('tempevent plimit', 2, NULL);
-INSERT INTO `command` VALUES ('tempevent location', 2, NULL);
-INSERT INTO `command` VALUES ('tempevent active', 2, NULL);
-INSERT INTO `command` VALUES ('tempevent join', 0, NULL);
-INSERT INTO `command` VALUES ('tempevent teleport', 2, NULL);
+REPLACE INTO `command` (`name`, `security`, `help`)
+VALUES
+    ('tempevent teleport', 1, 'Usage: .tempevent teleport. Teleports all players in temporary event queue to set location.'),
+    ('tempevent reset', 1, 'Usage: .tempevent reset. Resets all variables related to temporary event system so that a new one may be created.'),
+    ('tempevent plimit', 1, 'Usage: .tempevent plimit $amount. Sets maximum player limit for temporary event.'),
+    ('tempevent location', 1, 'Usage: .tempevent location. Sets the teleport location of the event to summon players to with the .tempevent teleport command.'),
+    ('tempevent leave', 0, 'Usage: .tempevent leave. Leaves the temporary event player is apart of.'),
+    ('tempevent join', 0, 'Usage: .tempevent join. Enter queue for an active temporary event. A Game Master will issue everyone in queue a summon once the event begins.'),
+    ('tempevent info', 0, 'Usage: .tempevent info. Gives information about the current temporary event, if there is one.'),
+    ('tempevent gminfo', 1, 'Not yet implemented.'),
+    ('tempevent create', 1, 'Not yet implemented.'),
+    ('tempevent active', 1, 'Usage: .tempevent active. Makes temporary event GM has prepared active so players may enter queue for it.');
--- sql/updates/dev_sql/TEMPEVENT/tempevent_strings.sql
+++ sql/updates/dev_sql/TEMPEVENT/tempevent_strings.sql
@@ -1,5 +1,12 @@
-INSERT INTO `skyfire_string` VALUES (59, 'There are currently no active events!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-INSERT INTO `skyfire_string` VALUES (60, 'This event has reached the maximum amount of participants.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-INSERT INTO `skyfire_string` VALUES (61, 'You have been added to the event queue. The event will start soon.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-INSERT INTO `skyfire_string` VALUES (62, 'Event status: [%u/%u] Players', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-INSERT INTO `skyfire_string` VALUES (63, 'You have been removed from the event queue.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+REPLACE INTO `skyfire_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`)
+VALUES
+    (59, 'There are currently no active events!', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    (60, 'This event has reached the maximum amount of participants.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    (61, 'You have been added to the event queue. The event will start soon.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    (62, 'Event status: [%u/%u] Players', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    (63, 'You have been removed from the event queue.', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    (64, 'TempEvent Info:', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    (65, 'Status: %s', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    (66, 'Location: Map(%s), Area(%s) X(%i) Y(%i) Z(%i)', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    (67, 'Player Limit: %u', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+    (68, 'Queued Players: %u', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);

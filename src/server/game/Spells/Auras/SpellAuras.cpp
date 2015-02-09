f1bb3469f31c8494a9a3379500590c497b30be39
Itslovelol <info@lovelol.de>
2015-02-09 11:42:17 +0100

    Core/SpellAuras: Reapply spell immunity if other immunity aura is still active.

--- src/server/game/Spells/Auras/SpellAuras.cpp
+++ src/server/game/Spells/Auras/SpellAuras.cpp
@@ -3728,6 +3728,19 @@ void Aura::HandleModMechanicImmunity(bool apply, bool Real)
 
     m_target->ApplySpellImmune(GetId(), IMMUNITY_MECHANIC, m_modifier.m_miscvalue, apply);
 
+    if (!apply)
+    {
+        Unit::AuraList mAuras = m_target->GetAurasByType(SPELL_AURA_MECHANIC_IMMUNITY);
+        for (Unit::AuraList::iterator iter = mAuras.begin(); iter != mAuras.end(); ++iter)
+        {
+            if ((*iter)->GetMiscValue() == GetMiscValue())
+            {
+                m_target->ApplySpellImmune((*iter)->GetId(), IMMUNITY_MECHANIC, (*iter)->GetMiscValue(), true);
+                break;
+            }
+        }
+    }
+
     // special cases
     switch (m_modifier.m_miscvalue)
     {

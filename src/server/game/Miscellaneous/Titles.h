9b54ec161740345733291b403147ed0c56a95559
Itslovelol <info@lovelol.de>
2015-03-11 22:22:07 +0100

    Core/Titles: Implement all titles up to Cataclysm.

--- src/server/game/Miscellaneous/Titles.h
+++ src/server/game/Miscellaneous/Titles.h
@@ -7,6 +7,7 @@
 
 const std::string titleStrings[] =
 {
+    // World Events
     "Brewmaster",
     "Elder",
     "FlameKepper",
@@ -17,7 +18,95 @@ const std::string titleStrings[] =
     "Patron",
     "thePilgrim",
     "theHallowed",
-    "theLoveFool"
+    "theLoveFool",
+
+    // Argent Tournament
+    "Crusader",
+    "ofDarnassus",
+    "ofGnomeregan",
+    "ofIronforge",
+    "ofOrgrimmar",
+    "ofSen'jin",
+    "ofSilvermoon",
+    "ofStormwind",
+    "oftheExodar",
+    "oftheUndercity",
+    "ofThunderBluff",
+
+    // Dungeons and Raids
+    "ArgentDefender",
+    ",BaneoftheFallenKing",
+    ",ChampionoftheFrozenWastes",
+    ",ChampionofUlduar",
+    ",ConquerorofUlduar",
+    ",HeraldoftheTitans",
+    "Jenkins",
+    "oftheNightfall",
+    "Starcaller",
+    "theAstralWalker",
+    "theImmortal",
+    "theKingslayer",
+    "theLightofDawn",
+    "theUndying",
+    "TwilightVanquisher",
+    ",Blackwing'sBane",
+    ",BlessedDefenderofNordrassil",
+    ",DefenderofaShatteredWorld",
+    ",Destroyer'sEnd",
+    "Firelord",
+    "Dragonslayer",
+    "oftheFourWinds",
+
+    // Dungeons and Raids: Realm Firsts
+    ",Conqueror of Naxxramas",
+    ",Death'sDemise",
+    "GrandCrusader",
+    "ObsidianSlayer",
+    "theCelestialDefender",
+    "theMagicSeeker",
+
+    // Miscellaneous
+    "Loremaster",
+    "thePatient",
+    "theExplorer",
+    "theSeeker",
+    "theCamel-Hoarder",
+    "theFlamebreaker",
+
+    // PvP Achievments
+    "oftheAlliance",
+    "oftheHorde",
+    ",theBloodthirsty",
+
+    // Reputation
+    "Ambassador",
+    "BloodsailAdmiral",
+    "GuardianofCenarius",
+    "oftheAshen Verdict",
+    "theArgent Champion",
+    "theDiplomat",
+    "theExalted",
+    "theInsane",
+    ",AvengerofHyjal",
+
+    // Battleground
+    "Battlemaster",
+    "HeroofTheAlliance",
+    "HerooftheHorde",
+    "Warbound",
+    "Warbringer",
+    "VeteranoftheAlliance",
+    "VeteranoftheHorde",
+
+    // Arena
+    "ArenaMaster",
+
+    // Professions
+    "Salty",
+    "Chef",
+    "Professor",
+    "AssistantProfessor",
+    "AssociateProfessor"
 };
 
 #endif

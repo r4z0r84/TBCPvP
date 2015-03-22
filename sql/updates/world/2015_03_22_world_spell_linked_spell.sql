1ec1ef899642607df489a622bed79c1865693666
Itslovelol <info@lovelol.de>
2015-03-22 21:14:00 +0100

    Database/Spells: Implement visual when summoning Warhorse / Charger.

---
+++ sql/updates/world/2015_03_22_world_spell_linked_spell.sql
@@ -0,0 +1,2 @@
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `comment`) VALUES ('23214', '31726', 'Summon Charger');
+INSERT INTO `spell_linked_spell` (`spell_trigger`, `spell_effect`, `comment`) VALUES ('13819', '31726', 'Summon Warhorse');

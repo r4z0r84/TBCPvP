0130198cd1acb3faf984cff51836806bb92fb064
itslovelol <robin.schriever.hude@web.de>
2015-04-14 14:30:40 +0200

    Database/Spell: Fanaticism now increases crit chance of Judgement of Command / Blood.

---
+++ sql/updates/world/sf_2015_04_14_spell_affect.sql
@@ -0,0 +1,5 @@
+UPDATE `spell_affect` SET `SpellFamilyMask`='2233382994944' WHERE (`entry`='31879') AND (`effectId`='0');
+UPDATE `spell_affect` SET `SpellFamilyMask`='2233382994944' WHERE (`entry`='31880') AND (`effectId`='0');
+UPDATE `spell_affect` SET `SpellFamilyMask`='2233382994944' WHERE (`entry`='31881') AND (`effectId`='0');
+UPDATE `spell_affect` SET `SpellFamilyMask`='2233382994944' WHERE (`entry`='31882') AND (`effectId`='0');
+UPDATE `spell_affect` SET `SpellFamilyMask`='2233382994944' WHERE (`entry`='31883') AND (`effectId`='0');

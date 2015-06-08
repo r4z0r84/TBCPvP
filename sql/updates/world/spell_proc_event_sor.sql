5a092d97732c98aa998bb30e7991df5726ba35be
robinsch <robin.schriever.hude@web.de>
2015-06-08 15:41:15 +0200

    Database/SpellProc: Allow SoR to proc on absorb.

---
+++ sql/updates/world/spell_proc_event_sor.sql
@@ -0,0 +1,16 @@
+INSERT INTO `spell_proc_event` (`entry`, `procFlags`, `procEx`) VALUES ('20154', '4', '1024');
+INSERT INTO `spell_proc_event` (`entry`, `procFlags`, `procEx`) VALUES ('21084', '4', '1024');
+INSERT INTO `spell_proc_event` (`entry`, `procFlags`, `procEx`) VALUES ('20287', '4', '1024');
+INSERT INTO `spell_proc_event` (`entry`, `procFlags`, `procEx`) VALUES ('20288', '4', '1024');
+INSERT INTO `spell_proc_event` (`entry`, `procFlags`, `procEx`) VALUES ('20289', '4', '1024');
+INSERT INTO `spell_proc_event` (`entry`, `procFlags`, `procEx`) VALUES ('20290', '4', '1024');
+INSERT INTO `spell_proc_event` (`entry`, `procFlags`, `procEx`) VALUES ('20291', '4', '1024');
+INSERT INTO `spell_proc_event` (`entry`, `procFlags`, `procEx`) VALUES ('20292', '4', '1024');
+INSERT INTO `spell_proc_event` (`entry`, `procFlags`, `procEx`) VALUES ('20293', '4', '1024');
+INSERT INTO `spell_proc_event` (`entry`, `procFlags`, `procEx`) VALUES ('27155', '4', '1024');
+
+UPDATE `spell_proc_event` SET `procFlags`='4', `procEx`='1024' WHERE entry IN (20154, 21084, 20287, 20288, 20289, 20290, 20291, 20292, 20293, 27155);
+
+
+
+
--- sql/updates/world/spell_proc_event_vindication.sql
+++ sql/updates/world/spell_proc_event_vindication.sql
@@ -1,3 +1,7 @@
+INSERT INTO `spell_proc_event` (`entry`, `procFlags`, `procEx`) VALUES ('9452', '20', '1024');
+INSERT INTO `spell_proc_event` (`entry`, `procFlags`, `procEx`) VALUES ('26016', '20', '1024');
+INSERT INTO `spell_proc_event` (`entry`, `procFlags`, `procEx`) VALUES ('26021', '20', '1024');
+
 UPDATE `spell_proc_event` SET `procFlags`='20', `procEx`='1024' WHERE (`entry`='9452');
 UPDATE `spell_proc_event` SET `procFlags`='20', `procEx`='1024' WHERE (`entry`='26016');
 UPDATE `spell_proc_event` SET `procFlags`='20', `procEx`='1024' WHERE (`entry`='26021');

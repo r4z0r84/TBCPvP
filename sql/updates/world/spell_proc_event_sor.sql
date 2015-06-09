e0188a094acd896c48a215d803c303f29e2394f5
robinsch <robin.schriever.hude@web.de>
2015-06-09 11:47:42 +0200

    Database/SpellProc: Allow SoR to proc on absorb.

--- sql/updates/world/spell_proc_event_sor.sql
+++ sql/updates/world/spell_proc_event_sor.sql
@@ -9,7 +9,7 @@ INSERT INTO `spell_proc_event` (`entry`, `procFlags`, `procEx`) VALUES ('20292',
 INSERT INTO `spell_proc_event` (`entry`, `procFlags`, `procEx`) VALUES ('20293', '4', '1024');
 INSERT INTO `spell_proc_event` (`entry`, `procFlags`, `procEx`) VALUES ('27155', '4', '1024');
 
-UPDATE `spell_proc_event` SET `procFlags`='4', `procEx`='1024' WHERE entry IN (20154, 21084, 20287, 20288, 20289, 20290, 20291, 20292, 20293, 27155);
+UPDATE `spell_proc_event` SET `procFlags`='4', `procEx`='1025', `SchoolMask`='1' WHERE entry IN (20154, 21084, 20287, 20288, 20289, 20290, 20291, 20292, 20293, 27155);
 
 
 
